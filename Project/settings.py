8d54e20416445ca2277b1f0f0192e0f77358e85d
whiteking24 <iniyagr@gmail.com>
2025-08-15 22:39:50 +0530

    Adding requirements text file to deploy in render

--- Project/settings.py
+++ Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$0@%oz04$)9(#eo0egf*qc)l&68afecr+hy7!5t1!7erorsk64
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['food-ordering-sytem.onrender.com','localhost']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
